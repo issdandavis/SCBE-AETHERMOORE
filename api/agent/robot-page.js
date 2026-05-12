892fb20f764a963cf8bba159a8cbc00f74241eb3
issdandavis <7795+issdandavis@users.noreply.github.com>
2026-05-12 13:09:33 -0700

    fix(vercel): serve ai guidance pages

---
+++ api/agent/robot-page.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const { serveDocsHtml } = require('./_static_file');
+
+module.exports = async function handler(req, res) {
+  return serveDocsHtml(req, res, 'robot.html', 'robot_page_unavailable');
+};
